339e2dfb7f0f1cbbc52a5e67af6bb5289861a822
clone12 <clones007@gmail.com>
2022-11-21 11:07:41 +0100

    Zadanie 5

--- Assets/Scripts/UI/UIMenuController.cs
+++ Assets/Scripts/UI/UIMenuController.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+using System.Collections.Generic;
+
 namespace TKOU.SimAI.UI
 {
     /// <summary>
@@ -9,6 +14,8 @@ namespace TKOU.SimAI.UI
     public class UIMenuController : MonoBehaviour
     {
         #region Variables
+
+        public delegate void ButtonAction();
 
         [SerializeField]
         private GameController gameController;
@@ -22,6 +29,9 @@ namespace TKOU.SimAI.UI
         [SerializeField]
         private GameObject mainMenuObject;
 
+        [SerializeField] List<Button> menuButtons = new List<Button>();
+        private int idx = 0;
+
         [SerializeField]
         [Tooltip("Set to TRUE to automatically start the game on play. Useful for testing.")]
         private bool autostart = false;
@@ -32,11 +42,16 @@ namespace TKOU.SimAI.UI
 
         private void Awake()
         {
+            menuButtons.Add(playButton);
+            menuButtons.Add(quitButton);
+
+            /*
             playButton.onClick.AddListener(PlayButton_OnClick);
             quitButton.onClick.AddListener(QuitButton_OnClick);
 
             gameController.OnGameRun += GameController_OnGameRun;
             gameController.OnGameEnd += GameController_OnGameEnd;
+            */
         }
 
         private void Start()
@@ -45,6 +60,17 @@ namespace TKOU.SimAI.UI
             {
                 gameController.RunGame();
             }
+
+            /*
+            menuButtons[0].image = GameObject.Find("UI-Button-Play").GetComponent<Image>();
+            menuButtons[0].image.color = Color.yellow;
+            playButton.onClick.AddListener(PlayButton_OnClick);
+
+            menuButtons[1].image = GameObject.Find("UI-Button-Quit").GetComponent<Image>();
+            menuButtons[1].image.color = Color.yellow;
+            playButton.onClick.AddListener(PlayButton_OnClick);
+            */
+
         }
 
         #endregion Unity methods
@@ -52,6 +78,21 @@ namespace TKOU.SimAI.UI
         #region Private methods
 
         #region Event callbacks
+
+        private void Update()
+        {
+            // Z powodu braku du¿ej iloœci czasu postanowi³em jedynie opisaæ rozwi¹zanie. 
+            // Tworzê listê przycisków oraz zmienn¹ idx
+            // Po kliknieciu strza³ki zmieniam wartoœæ idx maksymalnie do wielkoœci listy przycisków
+            // Przycisk o danym indeksie w liœcie staje siê wówczas aktywny i po naciœniêciu "entera" w³¹czam metode
+            // OnClick();
+            // Najlepiej jest zrobiæ now¹ klasê i wykonywaæ j¹ zarówno w "UIMenuController" oraz "UIButtonGame" i w inicjalizacji
+            // Pobierac wszystkie dostêpne przyciski i dodawac do listy
+            // Dodatkowo zauwazylem ze gdy wlaczone jest samo menu to raczej nie powinien byc widoczny pasek z "quit" na samym dole
+            // powinien byc widoczny dopiero w rozgrywce
+
+
+        }
 
         private void QuitButton_OnClick()
         {
@@ -82,4 +123,6 @@ namespace TKOU.SimAI.UI
 
         #endregion Private methods
     }
+
+
 }
